a044c501ba0acab6e15d4b2b6b60fb1b7067dcb3
sebastian lapt <smoreira11@hotmail.com>
2025-07-10 08:02:02 +0900

    add

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,4 +1,4 @@
-﻿// the ourAnimals array will store the following: 
+﻿// the ourAnimals array will store the following:
 string animalSpecies = "";
 string animalID = "";
 string animalAge = "";
@@ -23,59 +23,60 @@ string[][] jaggedArray = new string[][]
     new string[] { "one5", "two5", "three5", "four5", "five5", "six5" },
     new string[] { "one6", "two6", "three6", "four6", "five6", "six6" },
     new string[] { "one7", "two7", "three7", "four7", "five7", "six7" },
-    new string[] { "one8", "two8", "three8", "four8", "five8", "six8" }
+    new string[] { "one8", "two8", "three8", "four8", "five8", "six8" },
 };
 
 do
 {
-
-for (int i = 0; i < maxPets; i++) {
-
-
-if (ourAnimals[i, 0] != "ID #: ")
+    for (int i = 0; i < maxPets; i++)
     {
-        Console.WriteLine(ourAnimals[i, 0]);
+        if (ourAnimals[i, 0] != "ID #: ")
+        {
+            Console.WriteLine(ourAnimals[i, 0]);
 
-switch (menuSelection)
-{
-    case "1":
-        // List all of our current pet information
-        Console.WriteLine("this app feature is coming soon - please check back to see progress.");
-        Console.WriteLine("Press the Enter key to continue.");
-        readResult = Console.ReadLine();
-        break;
+            switch (menuSelection)
+            {
+                case "1":
+                    // List all of our current pet information
+                    Console.WriteLine(
+                        "this app feature is coming soon - please check back to see progress."
+                    );
+                    Console.WriteLine("Press the Enter key to continue.");
+                    readResult = Console.ReadLine();
+                    break;
 
-    case "2":
-        // List all of our current pet information
-        Console.WriteLine("this app feature is coming soon - please check back to see progress.");
-        Console.WriteLine("Press the Enter key to continue.");
-        readResult = Console.ReadLine();
-        break;
+                case "2":
+                    // List all of our current pet information
+                    Console.WriteLine(
+                        "this app feature is coming soon - please check back to see progress."
+                    );
+                    Console.WriteLine("Press the Enter key to continue.");
+                    readResult = Console.ReadLine();
+                    break;
 
-       case "3":
-       Console.WriteLine("Challenge Project - please check back soon to see progress.");
-       Console.WriteLine("Press the Enter key to continue.");
-       readResult = Console.ReadLine();
+                case "3":
+                    Console.WriteLine(
+                        "Challenge Project - please check back soon to see progress."
+                    );
+                    Console.WriteLine("Press the Enter key to continue.");
+                    readResult = Console.ReadLine();
 
-        break;
+                    break;
 
-    case "4":
-    Console.WriteLine("UNDER CONSTRUCTION - please check back next month to see progress.");
-    Console.WriteLine("Press the Enter key to continue.");
-    readResult = Console.ReadLine();
+                case "4":
+                    Console.WriteLine(
+                        "UNDER CONSTRUCTION - please check back next month to see progress."
+                    );
+                    Console.WriteLine("Press the Enter key to continue.");
+                    readResult = Console.ReadLine();
 
-        break;
+                    break;
 
-     default:
-        break;
-      }
-         }
-         }
-
-          } while (menuSelection != "exit");
+                default:
+                    break;
+            }
+        }
+    }
+} while (menuSelection != "exit");
 
 // Inicialización de datos de ejemplo (opcional)
-
-
-
-
--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec252b9d1e5dd2a32cc8148750280f7cb773a794")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18066df4fa2986df5266ac7e100afc39d56da30a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
